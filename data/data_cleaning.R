9e2e8dba6ddf5a1bc558301f2d5ff92ee7121f17
Michelle Huang <mh52@princeton.edu>
2022-09-19 00:12:43 -0400

    data cleaning R code

--- data/data_cleaning.R
+++ data/data_cleaning.R
@@ -1,1 +1,8 @@
 # DATA CLEANING
+
+library(tidyverse)
+
+# read in dataset: no_duplicate_dataset.csv
+dataset <- read.csv(file = 'https://raw.githubusercontent.com/BlazerYoo/wildlife-caption/mh-data-analysis/data/no_duplicate_dataset.csv?token=GHSAT0AAAAAABYYWKV27NM4TBFE23WBTU6IYZH6XLA')
+
+dataset
