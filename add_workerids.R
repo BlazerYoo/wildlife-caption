4cf1fcca88e03eecf6fae5057ccf57ffb7fc1892
Boaz Yoo <blazeryoo1921@gmail.com>
2022-09-18 21:14:43 -0400

    add worker id

---
+++ add_workerids.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+
+# Change working directory to encrypted drive
+setwd("E:/drive-download-20220919T004420Z-001")
+
+# Read csvs
+qualtrics <- read.csv('E:/drive-download-20220919T004420Z-001/no_id_full_data.csv')
+mturk <- read.csv('E:/drive-download-20220919T004420Z-001/mturk_workerids.csv')
+
+# Rename Answer.surveycode column in mturk
+names(mturk)[names(mturk) == "Answer.surveycode"] <- "Random.ID"
+
+# Add WorkerId column to qualtrics
+#   https://stackoverflow.com/questions/66854059/adding-a-new-element-in-a-column-based-on-a-value-of-another-column-in-r
+merged_data <- merge(qualtrics, mturk, by = "Random.ID", all = TRUE)
+
+# Remove rows with NA in columns
+#   https://stackoverflow.com/questions/11254524/omit-rows-containing-specific-column-of-na
+merged_data <- na.omit(merged_data)
+
+# Move WorkerId column to beginning
+#   https://stackoverflow.com/questions/22286419/move-a-column-to-first-position-in-a-data-frame
+merged_data <- merged_data %>%
+  select("WorkerId", everything())
+
+# Export dataframe as csv
+write.csv(merged_data, 'E:/merged.csv', row.names=FALSE)
