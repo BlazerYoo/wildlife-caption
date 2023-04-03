b5b6fc2fcb070894e6774546f1a8b9227d7b42d6
Boaz Yoo <blazeryoo1921@gmail.com>
2023-04-03 00:15:36 -0400

    Update generate_plots.R

--- stacked bar plot/generate_plots.R
+++ stacked bar plot/generate_plots.R
@@ -6,7 +6,7 @@ library(ggrepel)
 
 
 # Read in dataset
-dataset <- read.csv(file = 'https://raw.githubusercontent.com/BlazerYoo/wildlife-caption/main/data/data_finished_surveys.csv?token=GHSAT0AAAAAAB4XGWTD2P7HSRYPQZSYTWEGY6B3HJQ')
+dataset <- read.csv(file = 'https://raw.githubusercontent.com/BlazerYoo/wildlife-caption/main/data/data_finished_surveys.csv?token=GHSAT0AAAAAAB4XGWTCFVUK3RUUPRZA6GJIY6FL74A')
 
 
 # Treatment: image #
@@ -28,7 +28,15 @@ loris_treatments <- loris_data$Treatment.shown
 
 
 # Generate plot
-generate_plot <- function(q, animal, data, treatments, threshold){
+generate_plot <- function(q, animal, data, treatments, plot_args){
+  
+  threshold <- plot_args[1]
+  size <- plot_args[2]
+  padding <- plot_args[3]
+  radius <- plot_args[4]
+  border <- plot_args[5]
+  nudge <- plot_args[6]
+  
   
   # Truncate extended decimal values
   trunc_number_n_decimals <- function(numberToTrunc, nDecimals){
@@ -39,7 +47,7 @@ generate_plot <- function(q, animal, data, treatments, threshold){
     return(truncatedNumber)
   }
   
-  # Selectr 
+  # Dataframe with just treatments and responses to given question
   question <- data %>% select(q)
   question_df <- data.frame(question, treatments)
   
@@ -74,17 +82,29 @@ generate_plot <- function(q, animal, data, treatments, threshold){
   title <- gsub(".", " ", q, fixed=TRUE) %>% substring(5)
   
   # Normalized barplot
-  ggplot(df, aes(x=Image, y=Freq, fill=Score)) + 
+  plot <- ggplot(df, aes(x=Image, y=Freq, fill=Score)) + 
     geom_bar(position=position_stack(reverse = TRUE), stat="identity", color="black", width=0.9) +
     scale_fill_manual("Response", values=park_palette(n=5, name="Acadia"), 
                       labels=c("Strongly Disagree", "Disagree", "Neutral", "Agree", "Strongly Agree"),
                       guide=guide_legend(reverse=TRUE)) +
-    geom_label(aes(label = paste0(Freq*100,"%")), position=position_fill(vjust = 0.5, reverse = FALSE), size = 4,
-               color = ifelse(df$Freq >= threshold, "black", rgb(0, 0, 0, alpha = 0)),
-               fill = ifelse(df$Freq >= threshold, "white", rgb(0, 0, 0, alpha = 0)), show.legend = FALSE) +
-    geom_label_repel(aes(label = paste0(Freq*100,"%")), position=position_fill(vjust = 0.2, reverse = FALSE), size = 4,
-                     color = ifelse(df$Freq < threshold, "black", rgb(0, 0, 0, alpha = 0)),
-                     fill = ifelse(df$Freq < threshold, "white", rgb(0, 0, 0, alpha = 0)), show.legend = FALSE) +
+    
+    geom_label(aes(label = paste0(Freq*100,"%")),
+               position=position_fill(vjust = 0.5, reverse = TRUE),
+               label.padding = unit(padding, "lines"),
+               label.r = unit(radius, "lines"),
+               label.size = border,
+               size = size,
+               color = ifelse(df$Freq > threshold, "black", rgb(0, 0, 0, alpha = 0)),
+               fill= ifelse(df$Freq > threshold, "white", rgb(0, 0, 0, alpha = 0))) +
+    
+    geom_label(aes(label = paste0(Freq*100,"%")),
+               nudge_x = ifelse(df$Freq < threshold, nudge, 0),
+               label.padding = unit(padding, "lines"),
+               label.r = unit(radius, "lines"),
+               label.size = border,
+               size = size,
+               color = ifelse(df$Freq < threshold, "black", rgb(0, 0, 0, alpha = 0)),
+               fill = ifelse(df$Freq < threshold, "white", rgb(0, 0, 0, alpha = 0))) +
     xlab("Image") +
     ylab("Percentage of Respondents") +
     scale_y_continuous(labels=scales::percent) +
@@ -92,8 +112,12 @@ generate_plot <- function(q, animal, data, treatments, threshold){
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5), panel.border = element_blank(), panel.grid.major = element_blank(),
           panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"))
+  
+  
+  
+  #ggsave(paste(title, ".jpg", sep=""))
 
-  #return(df)
+  return(plot)
 }
 
 
@@ -103,18 +127,48 @@ generate_plot <- function(q, animal, data, treatments, threshold){
 #X...I.would.like.to.have.this.animal.as.a.pet.
 
 # Label staggering bar height threshold
-# Stagger labels for bar sections that represent less than 15%
-threshold = 0.15
+threshold = 0.03 # Stagger labels for bar sections that represent less than 3%
+size = 5
+padding = 0.2
+radius = 0.05
+border = 0.5
+nudge = 0.3
+
+plot_args <- c(threshold, size, padding, radius, border, nudge)
 
 # Generate plots for gorilla posts
-generate_plot("X...This.post.depicts.wildlife.research.", "Gorilla", gorilla_data, gorilla_treatments, threshold)
-generate_plot("X...I.would.seek.out.an.experience.to.personally.interact.with.this.animal.", "Gorilla", gorilla_data, gorilla_treatments, threshold)
-generate_plot("X...This.animal.would.make.a.good.pet.", "Gorilla", gorilla_data, gorilla_treatments, threshold)
-generate_plot("X...I.would.like.to.have.this.animal.as.a.pet.", "Gorilla", gorilla_data, gorilla_treatments, threshold)
+plot1 <- generate_plot("X...This.post.depicts.wildlife.research.", "Gorilla", gorilla_data, gorilla_treatments, plot_args)
+plot1
+plot2 <- generate_plot("X...I.would.seek.out.an.experience.to.personally.interact.with.this.animal.", "Gorilla", gorilla_data, gorilla_treatments, plot_args)
+plot3 <- generate_plot("X...This.animal.would.make.a.good.pet.", "Gorilla", gorilla_data, gorilla_treatments, plot_args)
+plot4 <- generate_plot("X...I.would.like.to.have.this.animal.as.a.pet.", "Gorilla", gorilla_data, gorilla_treatments, plot_args)
+
+
+# General panel of plots for gorilla posts
+figure <- ggarrange(plot1, plot2, plot3, plot4,
+                    common.legend = TRUE, legend = "bottom",
+                    ncol = 2, nrow = 2)
+figure <- annotate_figure(
+  figure,
+  top = text_grob("Gorilla Posts", face = "bold", size = 25)
+)
+figure
+
 
 
 # Generate plots for loris posts
-generate_plot("X...This.post.depicts.wildlife.research.", "Loris", loris_data, loris_treatments, threshold)
-generate_plot("X...I.would.seek.out.an.experience.to.personally.interact.with.this.animal.", "Loris", loris_data, loris_treatments, threshold)
-generate_plot("X...This.animal.would.make.a.good.pet.", "Loris", loris_data, loris_treatments, threshold)
-generate_plot("X...I.would.like.to.have.this.animal.as.a.pet.", "Loris", loris_data, loris_treatments, threshold)
+plot1 <- generate_plot("X...This.post.depicts.wildlife.research.", "Loris", loris_data, loris_treatments, plot_args)
+plot2 <- generate_plot("X...I.would.seek.out.an.experience.to.personally.interact.with.this.animal.", "Loris", loris_data, loris_treatments, plot_args)
+plot3 <- generate_plot("X...This.animal.would.make.a.good.pet.", "Loris", loris_data, loris_treatments, plot_args)
+plot4 <- generate_plot("X...I.would.like.to.have.this.animal.as.a.pet.", "Loris", loris_data, loris_treatments, plot_args)
+
+
+# General panel of plots for gorilla posts
+figure <- ggarrange(plot1, plot2, plot3, plot4,
+                    common.legend = TRUE, legend = "bottom",
+                    ncol = 2, nrow = 2)
+figure <- annotate_figure(
+  figure,
+  top = text_grob("Loris Posts", face = "bold", size = 25)
+)
+figure
